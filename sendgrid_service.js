0bad1d4173e98077830e495ab0c2e7af89c7cb4c
Sam Perez <samuel.e.perez@gmail.com>
2018-01-05 20:03:23 +0000

    Add support for multiple email addresses separated by commas

--- sendgrid_service.js
+++ sendgrid_service.js
@@ -1,23 +1,32 @@
+const R = require('ramda');
 const sendgrid = require('sendgrid');
 
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
-const SENDGRID_TO_EMAIL = process.env.SENDGRID_TO_EMAIL;
+const SENDGRID_TO_EMAIL = R.pipe(
+  R.split(','),
+  R.map((email) => ({email}))
+)(process.env.SENDGRID_TO_EMAIL);
 const SENDGRID_FROM_EMAIL = 'updates@fb-targeting-changes.com'
 
 const sendStatusEmail = (emailContent) => {
-  const helper = sendgrid.mail;
-  const from_email = new helper.Email(SENDGRID_FROM_EMAIL);
-  const to_email = new helper.Email(SENDGRID_TO_EMAIL);
-  const subject = `Daily Fb Targeting Changes Update`;
-  const content = new helper.Content('text/plain', emailContent);
-  const mail = new helper.Mail(from_email, subject, to_email, content);
-  
   const sg = sendgrid(SENDGRID_API_KEY);
+  
+  const subject = `Daily Fb Targeting Changes Update`;
 
   const request = sg.emptyRequest({
     method: 'POST',
     path: '/v3/mail/send',
-    body: mail.toJSON(),
+    body: {
+      from: {email: SENDGRID_FROM_EMAIL},
+      personalizations: [{
+        to: SENDGRID_TO_EMAIL
+      }],
+      subject,
+      content: [{
+        type: 'text/plain',
+        value: emailContent
+      }]
+    },
   });
   
   sg.API(request, function(error, response) {
@@ -28,4 +37,3 @@ const sendStatusEmail = (emailContent) => {
 };
 
 exports.sendStatusEmail = sendStatusEmail;
-
